f0c30a211075980a439461d5ebd47fad20e520c3
Yuma Endo <endo@ai-ms.com>
2025-01-21 16:44:46 +0900

    Update shared memory ipc implementation

--- src/shared_memory.cpp
+++ src/shared_memory.cpp
@@ -2,6 +2,9 @@
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <atomic>
+#include <chrono>
+#include <csignal>
 #include <cstring>
 #include <iostream>
 #include <stdexcept>
@@ -39,23 +42,35 @@ void SharedMemory::receive()
 {
     const char* shnName = "/shared_memory";
     const size_t bufferSize = 1024;
+    std::atomic<bool> running(true);
 
-    int shmFd = shm_open(shnName, O_RDONLY, 0);
-    if (shmFd == -1) {
-        std::cerr << "shm_open failed: " << strerror(errno) << std::endl;
-        return;
+    auto signalHandler = [](int) {
+        std::cout << "\nSignal received, exiting..." << std::endl;
+        exit(0);
+    };
+    std::signal(SIGINT, signalHandler);
+
+    while (running) {
+        int shmFd = shm_open(shnName, O_RDONLY, 0);
+        if (shmFd == -1) {
+            std::cerr << "shm_open failed: " << strerror(errno) << std::endl;
+            continue;
+        }
+
+        void* buffer = mmap(0, bufferSize, PROT_READ, MAP_SHARED, shmFd, 0);
+        if (buffer == MAP_FAILED) {
+            std::cerr << "mmap failed: " << strerror(errno) << std::endl;
+            close(shmFd);
+            continue;
+        }
+
+        const auto now = std::chrono::system_clock::now();
+        const auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch());
+        std::cout << "Received: " << static_cast<char*>(buffer) << "| time: " << duration.count() << std::endl;
+
+        munmap(buffer, bufferSize);
+        close(shmFd);
+
+        shm_unlink(shnName);
     }
-
-    void* buffer = mmap(0, bufferSize, PROT_READ, MAP_SHARED, shmFd, 0);
-    if (buffer == MAP_FAILED) {
-        std::cerr << "mmap failed: " << strerror(errno) << std::endl;
-        return;
-    }
-
-    std::cout << "Received: " << static_cast<char*>(buffer) << std::endl;
-
-    munmap(buffer, bufferSize);
-    close(shmFd);
-
-    shm_unlink(shnName);
 }

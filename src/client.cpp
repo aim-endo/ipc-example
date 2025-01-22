fdc3ef64f7caff6c6745055c48f2fa4b1e2dfde7
Yuma Endo <endo@ai-ms.com>
2025-01-22 12:02:37 +0900

    Update using gRPC

--- src/client.cpp
+++ src/client.cpp
@@ -1,34 +1,46 @@
-#include "ipc_base.h"
-#include "shared_memory.h"
-#include "grpc.h"
-#include "ucx.h"
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/client_context.h>
+#include <grpcpp/security/server_credentials.h>
+#include <grpcpp/support/status.h>
 #include <iostream>
 #include <memory>
 
+#include "hello.grpc.pb.h"
+
+class GreeterClient final {
+public:
+    GreeterClient(std::shared_ptr<grpc::Channel> channel)
+        : stub_(hello::Greeter::NewStub(channel)) {}
+
+    std::string SayHello(const std::string& name) {
+        hello::HelloRequest request;
+        request.set_name(name);
+
+        hello::HelloReply reply;
+        grpc::ClientContext context;
+
+        grpc::Status status = stub_->SayHello(&context, request, &reply);
+
+        if (status.ok())
+            return reply.message();
+        else {
+            std::cerr << "RPC failed: " << status.error_message() << std::endl;
+            return "RPC failed";
+        }
+    }
+
+private:
+    std::unique_ptr<hello::Greeter::Stub> stub_;
+};
+
 int main(const int argc, const char* argv[])
 {
-    if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " <ipc> <path>" << std::endl;
-        return 1;
-    }
+    auto channel = grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials());
+    GreeterClient client(channel);
 
-    const std::string method = argv[1];
-    const std::string path = argv[2];
-
-    std::unique_ptr<IPCBase> ipc;
-
-    if (method == "shared_memory") {
-        ipc = std::make_unique<SharedMemory>();
-    } else if (method == "grpc") {
-        ipc = std::make_unique<GRPC>();
-    } else if (method == "ucx") {
-        ipc = std::make_unique<UCX>();
-    } else {
-        std::cerr << "Invalid IPC method: " << method << std::endl;
-        return 1;
-    }
-
-    ipc->send(path);
+    // send & receive
+    const auto response = client.SayHello("world");
+    std::cout << "Greeter Reply: " << response << std::endl;
 
     return 0;
 }
--- src/daemon.cpp
+++ src/daemon.cpp
@@ -1,13 +1,33 @@
-#include "ipc_base.h"
-#include "shared_memory.h"
-#include "grpc.h"
-#include "ucx.h"
-#include <iostream>
-#include <memory>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/security/server_credentials.h>
+#include <grpcpp/server_builder.h>
+#include "hello.grpc.pb.h"
+
+class GreeterServiceImpl final : public hello::Greeter::Service {
+    grpc::Status SayHello(grpc::ServerContext* context, const hello::HelloRequest* request, hello::HelloReply* response) override
+    {
+        std::cout << "[server] Received request: " << request->name() << std::endl;
+        std::string prefix("Hello ");
+        response->set_message(prefix + request->name());
+        return grpc::Status::OK;
+    }
+};
+
+void RunServer()
+{
+    const std::string server_address("0.0.0.0:50051");
+    GreeterServiceImpl service;
+
+    grpc::ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+
+    std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+    server->Wait();
+}
 
 int main(void)
 {
-    std::unique_ptr<IPCBase> ipc = std::make_unique<SharedMemory>();
-    ipc->receive();
+    RunServer();
     return 0;
 }
